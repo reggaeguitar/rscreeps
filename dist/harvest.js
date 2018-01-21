d04f9cb975b432dd1cb7dbe8c58e87dd72c9b07f
Joe Brady <joseph.b.brady@gmail.com>
2018-01-21 14:13:23 -0600

    added getSources

--- dist/harvest.js
+++ dist/harvest.js
@@ -2,15 +2,19 @@ var _ = require('lodash');
 
 module.exports = {
     doHarvest: function (creep) {
+        var sources = this.getSources(creep);
         if (creep.harvest(sources[creep.memory.sourceToHarvest]) == ERR_NOT_IN_RANGE) {
             creep.moveTo(sources[creep.memory.sourceToHarvest], 
                 { visualizePathStyle: {stroke: '#ffaa00' } });
         }
     },
     startHarvest: function(creep) {
-        var sources = creep.room.find(FIND_SOURCES);
+        var sources = this.getSources(creep);
         var sourceToHarvest = _.random(0, sources.length - 1);
         creep.memory.sourceToHarvest = sourceToHarvest;
         this.doHarvest(creep);
+    },
+    getSources: function(creep) {
+        return creep.room.find(FIND_SOURCES);
     }
 };
