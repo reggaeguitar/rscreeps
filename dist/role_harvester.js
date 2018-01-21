77083d5472c0372d5e2c60b1018d2f0ed0ee02d9
Joseph Brady <escalade2005@hotmail.com>
2018-01-20 18:41:14 -0600

    fixed harvester role

--- dist/role_harvester.js
+++ dist/role_harvester.js
@@ -1,28 +1,27 @@
 var harvest = require('harvest');
 
-var roleUpgrader = {
+var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
-        }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.upgrading = true;
-            creep.say('⚡ upgrade');
-        }
-
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-            }
+        if(creep.carry.energy < creep.carryCapacity) {
+            harvest.doHarvest();
         }
         else {
-            harvest.doHarvest(creep);
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
         }
     }
 };
 
-module.exports = roleUpgrader;
+module.exports = roleHarvester;
