f0fb7c1c77ea50b74877837fc36a60d4590dae00
Joe Brady <joseph.b.brady@gmail.com>
2018-01-21 14:12:11 -0600

    added harvest.startHarvest

--- src/harvest.js
+++ src/harvest.js
@@ -2,21 +2,15 @@ var _ = require('lodash');
 
 module.exports = {
     doHarvest: function (creep) {
-        //if (creep.memory.harvesting === false) {
-        //    harvesting = true;
-        //    creep.memory.sourceIndex = _
-        //}
-        //if (creep.carry.energy == creep.energyCapacity) {
-        //    creep.memory.sourceIndex = -1;
-        //}
-        //if (creep.memory.sourceIndex == -1) {
-        //    var sources = creep.room.find(FIND_SOURCES);
-        //    creep.memory.sourceIndex = 
-        //}
-        var sources = creep.room.find(FIND_SOURCES);
-        var sourceToHarvest = 0;//_.random(0, sources.length);
-        if(creep.harvest(sources[sourceToHarvest]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[sourceToHarvest], {visualizePathStyle: {stroke: '#ffaa00'}});
+        if (creep.harvest(sources[creep.memory.sourceToHarvest]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[creep.memory.sourceToHarvest], 
+                { visualizePathStyle: {stroke: '#ffaa00' } });
         }
+    },
+    startHarvest: function(creep) {
+        var sources = creep.room.find(FIND_SOURCES);
+        var sourceToHarvest = _.random(0, sources.length - 1);
+        creep.memory.sourceToHarvest = sourceToHarvest;
+        this.doHarvest(creep);
     }
 };
