92ee97cf449d4c1cf1448ac8665fd2675ee12ab9
Joe Brady <joseph.b.brady@gmail.com>
2018-01-21 14:20:07 -0600

    changed upgrader to use worker

--- dist/role_upgrader.js
+++ dist/role_upgrader.js
@@ -1,26 +1,15 @@
-var harvest = require('harvest');
+var worker = require('role_worker');
 
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
-        }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.upgrading = true;
-            creep.say('⚡ upgrade');
-        }
-
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-            }
-        }
-        else {
-            harvest.doHarvest(creep);
+        workeer.run(creep, this.doWork);        
+    },
+    doWork: function(creep) {
+        if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller, 
+                { visualizePathStyle: { stroke: '#ffffff' } });
         }
     }
 };
