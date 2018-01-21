b06448bdee7ddea72babc65e775c24e885dee7f9
Joe Brady <joseph.b.brady@gmail.com>
2018-01-21 14:18:08 -0600

    changed energyCapacity to carryCapacity

--- dist/role_worker.js
+++ dist/role_worker.js
@@ -9,10 +9,10 @@ module.exports = {
             // done working, need to harvest again
             creep.memory.harvesting = true;
             harvest.startHarvest(creep);
-        } else if (creep.memory.harvesting && creep.carry.energy != creep.energyCapacity) {
+        } else if (creep.memory.harvesting && creep.carry.energy != creep.carryCapacity) {
             // harvesting, not full yet
             harvest.doHarvest(creep);
-        } else if (creep.memory.harvesting && creep.carry.energy == creep.energyCapacity) {
+        } else if (creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
             // harvesting, full
             creep.memory.harvesting = false;
             doWork(creep);
