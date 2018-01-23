57226d6680df0731a07787ad42dad801442114f3
Joe Brady <joseph.b.brady@gmail.com>
2018-01-22 18:51:52 -0600

    added extra creep bodies to spawn

--- dist/creepSpawn.js
+++ dist/creepSpawn.js
@@ -7,19 +7,25 @@ module.exports = {
             this.spawnCreepIfPossible(room, spawn);
         }
     },
+    bodies: [
+        [WORK, WORK, CARRY, MOVE, MOVE, MOVE],
+        [WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
+        [WORK, CARRY, CARRY, MOVE, MOVE],
+        [WORK, CARRY, MOVE, MOVE],
+        [WORK, CARRY, MOVE]
+    ],
     spawnCreepIfPossible: function(room, spawn) {
         if (spawn.spawning) {
             return;
         }
-        var bodyParts = [WORK, WORK, CARRY, MOVE, MOVE];
-        var costForCreep = this.creepCost(bodyParts);
-        if (costForCreep > room.energyCapacityAvailable) {
-            console.log('Error: body parts cost too much for spawner and extensions.');
-        }
-        if (room.energyAvailable >= costForCreep) {
-            var roles = util.getRoles();
-            var role = roles[_.random(roles.length - 1)];            
-            this.spawnCreepImpl(bodyParts, role, spawn);
+        for (let i = 0; i < this.bodies.length; i++) {
+            var bodyParts = this.bodies[i];
+            var costForCreep = this.creepCost(bodyParts);
+            if (room.energyAvailable >= costForCreep) {
+                var roles = util.getRoles();
+                var role = roles[_.random(roles.length - 1)];            
+                this.spawnCreepImpl(bodyParts, role, spawn);
+            }
         }
     },
     spawnCreepImpl: function(bodyParts, role, spawn) {
