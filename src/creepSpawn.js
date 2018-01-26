99c2ff7c27b3705b485fd3abdda9370f7af92715
Joe Brady <joseph.b.brady@gmail.com>
2018-01-25 22:00:14 -0600

    tweaks to get colony up and running again

--- src/creepSpawn.js
+++ src/creepSpawn.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var data = require('data');
+var _ = require('lodash');
 
 module.exports = {    
     run: function(room, spawn) {
@@ -7,35 +8,41 @@ module.exports = {
             return;
         }
         var energyAvailable = room.energyAvailable;
+        //console.log(energyAvailable);
         if (energyAvailable < 200) {
             console.log('room has less than 200 energy, can\'t spawn creep');
             return;
         }
         var creepCountsByRole = util.getCreepRoleCounts();
         var maxHarvesterCount = data.maxHarvesterCount;
-        if (creepCountsByRole['harvester'] < maxHarvesterCount) {
+        console.log(JSON.stringify(creepCountsByRole));
+        if (!creepCountsByRole.hasOwnProperty('harvester') || creepCountsByRole['harvester'] < maxHarvesterCount) {
             this.spawnBestHarvesterPossible(room, spawn);
         } else {
             var maxWorkerCount = data.maxWorkerCount;
             // todo loop over roles
-            var workerCount = creepCountsByRole['hauler'] +
-                              creepCountsByRole['builder'] +  
-                              creepCountsByRole['upgrader'];
-            if (workerCount < maxWorkerCount) {
-                this.spawnBestWorkerPossible(room, spawn, 'hauler');
+            if (creepCountsByRole.hasOwnProperty('harvester')) {
+                var workerCount = Object.keys(Game.creeps).length - creepCountsByRole['harvester'];
+                console.log(workerCount);
+                if (workerCount < maxWorkerCount) {
+                    var rand = _.random(1);
+                    this.spawnBestWorkerPossible(room, spawn, rand == 1 ? 'hauler' : 'upgrader');
+                }
             }
-        }        
+        }
     }, 
     spawnBestHarvesterPossible: function(room, spawn) {
+        console.log('spawnBestHarvesterPossible called');
         var moveCount = room.controller.level;
         var movePartsCost = BODYPART_COST[MOVE] * moveCount;
         var energyLeftForWorkParts = room.energyAvailable - movePartsCost;
         var workCount = energyLeftForWorkParts / BODYPART_COST[WORK];
+        console.log('workCount: ' + workCount);
         var bodyParts = this.getBodyPartsFromCounts(workCount, 0, moveCount);
         this.spawnCreepImpl(bodyParts, 'harvester', spawn);
     },
     spawnBestWorkerPossible: function(room, spawn, role) {
-        var half = energyAvailable / 2;
+        var half = room.energyAvailable / 2;
         var workCount = half / BODYPART_COST[WORK];
         if (BODYPART_COST[MOVE] != BODYPART_COST[CARRY]) {
             console.log('MOVE and CARRY no longer the same cost, update creepSpawn.js');
@@ -43,9 +50,9 @@ module.exports = {
         var carryAndMoveCount = (half / BODYPART_COST[MOVE]) / 2;
         var bodyParts = this.getBodyPartsFromCounts(
             workCount, carryAndMoveCount, carryAndMoveCount);
-        if (this.creepCost(bodyParts) > energyAvailable) {
+        if (this.creepCost(bodyParts) > room.energyAvailable) {
             console.log('error in creepSpawn, bodyParts: ' + JSON.stringify(bodyParts) 
-                + ' cost more than energyAvailable: ' + energyAvailable);
+                + ' cost more than energyAvailable: ' + room.energyAvailable);
         }
         this.spawnCreepImpl(bodyParts, role, spawn);
     },
