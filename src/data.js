42a0b6ec51b156ecab3c982f6f2aff5ff36b69f9
Joe Brady <joseph.b.brady@gmail.com>
2018-01-25 20:30:43 -0600

    adding hauler role to data

--- src/data.js
+++ src/data.js
@@ -1,12 +1,14 @@
 var roleHarvester = require('role_harvester');
 var roleUpgrader = require('role_upgrader');
 var roleBuilder = require('role_builder');
+var roleHauler = require('role_hauler');
 
 module.exports = {
     creepData: {
         'harvester': { roleObj: roleHarvester },
         'upgrader': { roleObj: roleUpgrader },
         'builder': { roleObj: roleBuilder },
+        'hauler': { roleObj: roleHauler },
     },
     maxHarvesterCount: 2,
     maxWorkerCount: 5,
