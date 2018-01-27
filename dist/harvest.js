7d014bcf6f4bd98f506e05183d2646422a1ee453
Joe Brady <joseph.b.brady@gmail.com>
2018-01-26 21:55:46 -0600

    added code so haulers will fill storage from containers

--- dist/harvest.js
+++ dist/harvest.js
@@ -3,11 +3,19 @@ var mapUtil = require('mapUtil');
 
 module.exports = {        
     doHarvest: function (creep) {
-        var closestStorageOrContainer = creep.pos.findClosestByRange(
-            FIND_STRUCTURES, { filter : s => 
-                (s.structureType == STRUCTURE_STORAGE || 
-                s.structureType == STRUCTURE_CONTAINER) &&
-                _.sum(s.store) > 200 });
+        var closestEnergyLocation;
+        if (creep.memory.role == 'hauler') {
+            closestEnergyLocation = creep.pos.findClosestByRange(
+                FIND_STRUCTURES, { filter : s => 
+                    s.structureType == STRUCTURE_CONTAINER &&
+                    _.sum(s.store) > 200 });
+        } else {
+            closestEnergyLocation = creep.pos.findClosestByRange(
+                FIND_STRUCTURES, { filter : s => 
+                    (s.structureType == STRUCTURE_STORAGE || 
+                    s.structureType == STRUCTURE_CONTAINER) &&
+                    _.sum(s.store) > 200 });
+        }
         if (closestStorageOrContainer != undefined) {
             if (creep.withdraw(closestStorageOrContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(closestStorageOrContainer);
