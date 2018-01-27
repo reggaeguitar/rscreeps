c0f2ec5afe76ca36b03d6bf8161344e253034ab2
Joe Brady <joseph.b.brady@gmail.com>
2018-01-27 13:48:41 -0600

    changed workers to only get energy from storage

--- dist/harvest.js
+++ dist/harvest.js
@@ -12,8 +12,7 @@ module.exports = {
         } else {
             closestEnergyLocation = creep.pos.findClosestByRange(
                 FIND_STRUCTURES, { filter : s => 
-                    (s.structureType == STRUCTURE_STORAGE || 
-                    s.structureType == STRUCTURE_CONTAINER) &&
+                    s.structureType == STRUCTURE_STORAGE &&
                     _.sum(s.store) > 200 });
         }
         if (closestEnergyLocation != undefined) {
