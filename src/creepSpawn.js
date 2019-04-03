fb4b9f7a1455a7409b942f1d3a2984a0533d9242
Joe Brady <joseph.b.brady@gmail.com>
2019-04-02 23:57:32 -0600

    fixed creepSpawn bug

--- src/creepSpawn.js
+++ src/creepSpawn.js
@@ -189,7 +189,7 @@ module.exports = {
             room.energyAvailable + ', can\'t spawn creep';
             if (data.log) console.log(message);
             if (data.notify) Game.notify(message);
-            return;
+            return true;
         }
     }
 };
