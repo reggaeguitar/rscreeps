f0f5d2f375a7ce5438dee4fcc7b97cc8a3413b0f
Joe Brady <joseph.b.brady@gmail.com>
2019-04-03 20:48:18 -0600

    rewrote posHasCreep part of isPassable

--- dist/creepUtil.js
+++ dist/creepUtil.js
@@ -20,10 +20,8 @@ module.exports = {
     isPassable: function(creep, newPos) {
         // is passable if plain and not occupied by a creep
         let terrain = Game.map.getRoomTerrain(creep.room.name).get(newPos.x, newPos.y);
-        let creepsInSameRoom = _.filter(Game.creeps, c => c.room.name == creep.room.name);
-        let creepsOtherThanThisOne = _.pull(Object.keys(creepsInSameRoom), creep.name);        
-        let posHasCreep = _.findIndex(creepsOtherThanThisOne,
-            c => Game.creeps[c].pos.isEqualTo(newPos)) != -1;
+        let creepsOnNewPos = _.filter(newPos.look(), x => x.type == 'creep');
+        let posHasCreep = creepsOnNewPos != undefined && creepsOnNewPos.length;
         return terrain == 'plain' && !posHasCreep;
     },    
 }
