81ed8a50da4f692b054ae79d1c909a033c32bdb9
Joe Brady <joseph.b.brady@gmail.com>
2020-06-12 23:29:11 -0600

    logging

--- dist/creepUtil.js
+++ dist/creepUtil.js
@@ -28,7 +28,7 @@ module.exports = {
         let terrain = Game.map.getRoomTerrain(creep.room.name).get(newPos.x, newPos.y);
         let creepsOnNewPos = _.filter(newPos.look(), x => x.type == 'creep');
         let posHasCreep = creepsOnNewPos != undefined && creepsOnNewPos.length;
-        logger.log('in isPassable', { terrain, creepsOnNewPos, posHasCreep });
+        logger.log('in isPassable', { terrain, creepsOnNewPosIsDefined: creepsOnNewPos != undefined, posHasCreep });
         return terrain == 'plain' && !posHasCreep;
     },    
 }
