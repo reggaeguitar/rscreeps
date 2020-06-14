0bb80a76faf0e78e80a261c4ff55c10130052298
Joe Brady <joseph.b.brady@gmail.com>
2020-06-13 19:51:42 -0600

    splitting harvesterSourceDecider into its own file

--- src/role/harvester.js
+++ src/role/harvester.js
@@ -1,6 +1,6 @@
 const roles = require('./role_roles');
-const logger = require('./logger');
 const _ = require('lodash');
+const harvesterSourceDecider = require('services_harvesterSourceDecider');
 
 module.exports = {
     run: function(creep) {
@@ -15,38 +15,7 @@ module.exports = {
             creep.moveTo(sources[creep.memory.sourceToHarvest], 
                 { visualizePathStyle: { stroke: '#ffaa00' } });
         }
-    },
-    decideWhichSourceToHarvest: function(harvesters, sourceCount) {
-        // find source with least count of harvesters assigned
-        const harvestersSources = harvesters.map(h => h.memory.sourceToHarvest);
-        const sourceCounts = _.countBy(harvestersSources, x => x);           
-        // assign the harvester to the source with no harvesters
-        const min = _.min(sourceCounts, x => x);
-        const entries = Object.entries(sourceCounts);
-        if (entries.length != sourceCount) {
-            // there is a source with no harvester assigned
-            // const allSources = 
-            function* generator() {
-                let index = sourceCount;
-                while (index > 0) {
-                    index = index - 1;
-                    yield index;
-                }
-              }
-            const allSources = Array.from(generator());
-            const keysAsInts = Object.keys(entries).map(x => +x);
-            const sourcesWithNoHarvestersAssigned = _.pull(allSources, ...keysAsInts);
-            return sourcesWithNoHarvestersAssigned[0];
-        }
-        const sourcesWithMinHarvestersAssigned = entries.filter(x => x[1] == min).map(x => +x[0]);
-        if (sourcesWithMinHarvestersAssigned.length == 1) return sourcesWithMinHarvestersAssigned[0];
-        // if there is a tie return the source with the oldest harvester assigned, aka soonest to die
-        const harvestersAtMinSources = harvesters.filter(x => 
-            sourcesWithMinHarvestersAssigned.includes(x.memory.sourceToHarvest));
-        const oldestAge = _.min(harvestersAtMinSources.map(x => x.ticksToLive));
-        const ret = harvestersAtMinSources.find(x => x.ticksToLive == oldestAge).memory.sourceToHarvest;
-        return ret;
-    },
+    },    
     startHarvest: function(creep, sources) {
         if (sources.length == 1) return 0;
         const creepsInSameRoom = _.filter(Game.creeps, c => c.room.name == creep.room.name);
@@ -57,7 +26,7 @@ module.exports = {
         } else {
             const harvesters = _.filter(creepsInSameRoom, c => c.memory.role == roles.RoleHarvester
                 && c.memory.sourceToHarvest != undefined);
-            sourceToHarvest = this.decideWhichSourceToHarvest(harvesters, source.length);
+            sourceToHarvest = harvesterSourceDecider.decideWhichSourceToHarvest(harvesters, source.length);
         }
         creep.memory.sourceToHarvest = sourceToHarvest;
     }
--- tests/harvesterSourceDeciderTests.js
+++ tests/harvesterSourceDeciderTests.js
@@ -1,17 +1,19 @@
-const test = require('./test.js');
-const harvester = require('../dist/role_harvester');
+const test = require('./test');
+const harvesterSourceDecider = require('../dist/services_harvesterSourceDecider');
 
 // this assumes there are two sources in the room
 const sourceCount = 2;
+
 // find source with least count of harvesters assigned
 // if there is a tie choose the one that has the oldest (soonest to die) harvester assigned
-
 
 (function harvester_decideWhichSourceToHarvest_oneOtherHarvester_assignsToOtherSource() {
     // arrange
     const creeps = [{ memory: { sourceToHarvest: 0 }, ticksToLive: 1500 }];
+
     // act
-    const result = harvester.decideWhichSourceToHarvest(creeps, sourceCount);
+    const result = harvesterSourceDecider.decideWhichSourceToHarvest(creeps, sourceCount);
+
     // assert
     const expected = 1;
     test.assertEqual(expected, result);
@@ -22,8 +24,10 @@ const sourceCount = 2;
     const creeps = [{ memory: { sourceToHarvest: 0 }, ticksToLive: 1500 }, 
                     { memory: { sourceToHarvest: 0 }, ticksToLive: 10 },
                     { memory: { sourceToHarvest: 1 }, ticksToLive: 10 }];
+
     // act
-    const result = harvester.decideWhichSourceToHarvest(creeps, sourceCount);
+    const result = harvesterSourceDecider.decideWhichSourceToHarvest(creeps, sourceCount);
+
     // assert
     const expected = 1;
     test.assertEqual(expected, result);
@@ -44,8 +48,10 @@ const sourceCount = 2;
         { memory: { sourceToHarvest: 17 }, ticksToLive: 10 },
         { memory: { sourceToHarvest: 23 }, ticksToLive: 10 },
     ];
+
     // act
-    const result = harvester.decideWhichSourceToHarvest(creeps, 6);
+    const result = harvesterSourceDecider.decideWhichSourceToHarvest(creeps, 6);
+
     // assert
     const expected = 17;
     test.assertEqual(expected, result);
@@ -55,8 +61,10 @@ const sourceCount = 2;
     // arrange
     const creeps = [{ memory: { sourceToHarvest: 0 }, ticksToLive: 1500 }, 
                     { memory: { sourceToHarvest: 1 }, ticksToLive: 10 }];
+                    
     // act
-    const result = harvester.decideWhichSourceToHarvest(creeps, sourceCount);
+    const result = harvesterSourceDecider.decideWhichSourceToHarvest(creeps, sourceCount);
+    
     // assert
     const expected = 1;
     test.assertEqual(expected, result);
