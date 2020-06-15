f906c416847f4ef9fbf628605bf40c0e15deed81
Joe Brady <joseph.b.brady@gmail.com>
2020-06-14 23:52:02 -0600

    bugfix

--- dist/services_maxWorkerCount.js
+++ dist/services_maxWorkerCount.js
@@ -8,7 +8,7 @@ module.exports = {
         const droppedEnergy = room.find(FIND_DROPPED_RESOURCES)
             .filter(x => x.resourceType = RESOURCE_ENERGY);
         const totalDroppedEnergy = _.sum(droppedEnergy, x => x.amount);
-        if (totalDroppedEnergy > room.controller.level * this) {
+        if (totalDroppedEnergy > room.controller.level * this.energyThreshold) {
             const curWorkerCount = util.workerCount(room.name, creepCountsByRole);
             return curWorkerCount + 1;
         }
