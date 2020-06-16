97f996bb20a9532fdb21d6579826c7b1534eab73
Joe Brady <joseph.b.brady@gmail.com>
2020-06-15 22:39:39 -0600

    fixing spawn logic to only spawn workers if there's a lot of energy available

--- dist/services_maxWorkerCount.js
+++ dist/services_maxWorkerCount.js
@@ -4,11 +4,14 @@ const _ = require('lodash');
 
 module.exports = {
     energyThreshold: 150,
+    energyAvailableThreshold: 0.8,
     maxWorkerCount: function(room, creepCountsByRole) {
         const droppedEnergy = room.find(FIND_DROPPED_RESOURCES)
             .filter(x => x.resourceType = RESOURCE_ENERGY);
         const totalDroppedEnergy = _.sum(droppedEnergy, x => x.amount);
-        if (totalDroppedEnergy > room.controller.level * this.energyThreshold) {
+        const lotsOfDroppedEnergy = totalDroppedEnergy > room.controller.level * this.energyThreshold;
+        const lotsOfAvailableEnergy = room.energyAvailable >= room.energyCapacityAvailable * this.energyAvailableThreshold;
+        if (lotsOfDroppedEnergy && lotsOfAvailableEnergy) {
             const curWorkerCount = util.workerCount(room.name, creepCountsByRole);
             return curWorkerCount + 1;
         }
--- tests/maxWorkerCount.tests.js
+++ tests/maxWorkerCount.tests.js
@@ -8,6 +8,10 @@ module.exports = {
         const energyThreshold = 150;
         test.assertEqual(energyThreshold, maxWorkerCount.energyThreshold);
     },
+    maxWorkerCount_maxWorkerCount_energyAvailableThreshold_setProperly: function() {
+        const energyAvailableThreshold = 0.8;
+        test.assertEqual(energyAvailableThreshold, maxWorkerCount.energyAvailableThreshold);
+    },
     maxWorkerCount_maxWorkerCount_notEnoughDroppedEnergy_returnsSixForLevelOneController: () => {
         // arrange
         const roomFindDroppedResourcesReturn = [
@@ -84,7 +88,7 @@ module.exports = {
         const expected = 8;
         test.assertEqual(expected, result);
     },
-    maxWorkerCount_maxWorkerCount_lotsOfEnergyInPiles_returnsCurrentWorkerCountPlusOne: () => {
+    maxWorkerCount_maxWorkerCount_lotsOfEnergyInPiles_andLotsAvailable_returnsCurrentWorkerCountPlusOne: () => {
         // energy threshold = room.controller.level * 50
         // arrange
         const roomFindDroppedResourcesReturn = [
@@ -132,20 +136,24 @@ module.exports = {
         const roomMock = { 
             find: () => roomFindDroppedResourcesReturn,
             controller: { level: 2 },
-            name: roomName
+            name: roomName,
+            energyAvailable: 500,
+            energyCapacityAvailable: 550
         };
-
+        
         const creepCountsByRoleMock = { foo: "bar" };
-
+        
         const curWorkerCount = 8;
+        // since 500 * energyAvailableThreshold 0.8 > 550 (room energyCapacity) increase worker count
         const expected = curWorkerCount + 1;
-
+        
         util.workerCount = (roomNameArg, creepCountsByRoleArg) => {
             if (roomNameArg !== roomName || creepCountsByRoleArg !== creepCountsByRoleMock) {
-                return undefined;
+                return null;
             }
             return curWorkerCount;
-        };
+        };        
+       
     
         // act
         const result = maxWorkerCount.maxWorkerCount(roomMock, creepCountsByRoleMock);
@@ -153,25 +161,76 @@ module.exports = {
         // assert
         test.assertEqual(expected, result);
     },
-    // if no dropped energy return 6 for controller level 1 otherwise 8
-}
+    maxWorkerCount_maxWorkerCount_lotsOfEnergyInPiles_butNotLotsAvailable_returnsCurrentWorkerCount: () => {
+        // energy threshold = room.controller.level * 50
+        // arrange
+        const roomFindDroppedResourcesReturn = [
+            {
+              "room": {
+                "name": "W18S31",
+                "energyAvailable": 550,
+                "energyCapacityAvailable": 550,
+                "visual": {
+                  "roomName": "W18S31"
+                }
+              },
+              "pos": {
+                "x": 7,
+                "y": 29,
+                "roomName": "W18S31"
+              },
+              "id": "5ee6f0d63e1ffd5923e44397",
+              "energy": 226,
+              "amount": 226,
+              "resourceType": "energy"
+            },
+            {
+              "room": {
+                "name": "W18S31",
+                "energyAvailable": 550,
+                "energyCapacityAvailable": 550,
+                "visual": {
+                  "roomName": "W18S31"
+                }
+              },
+              "pos": {
+                "x": 10,
+                "y": 27,
+                "roomName": "W18S31"
+              },
+              "id": "5ee6f0dd5c6dd4ba455e8167",
+              "energy": 80,
+              "amount": 80,
+              "resourceType": "energy"
+            },            
+        ];
 
-// {
-//     "room": {
-//         "name": "W18S31",
-//         "energyAvailable": 550,
-//         "energyCapacityAvailable": 550,
-//         "visual": {
-//         "roomName": "W18S31"
-//         }
-//     },
-//     "pos": {
-//         "x": 7,
-//         "y": 29,
-//         "roomName": "W18S31"
-//     },
-//     "id": "5ee6f0d63e1ffd5923e44397",
-//     "energy": 0, // todo is this actually 0 when it's a different resource type?
-//     "amount": 26,
-//     "resourceType": "someOtherResource"
-//     },
+        const roomName = 'E17W34';
+        const roomMock = { 
+            find: () => roomFindDroppedResourcesReturn,
+            controller: { level: 2 },
+            name: roomName,
+            energyAvailable: 100,
+            energyCapacityAvailable: 550
+        };
+
+        const creepCountsByRoleMock = { foo: "bar" };
+
+        const curWorkerCount = 8;
+        // since 100 * energyAvailableThreshold 0.8 < 550 (room energyCapacity) don't increase worker count
+        const expected = curWorkerCount;
+
+        util.workerCount = (roomNameArg, creepCountsByRoleArg) => {
+            if (roomNameArg !== roomName || creepCountsByRoleArg !== creepCountsByRoleMock) {
+                return null;
+            }
+            return curWorkerCount;
+        };
+
+        // act
+        const result = maxWorkerCount.maxWorkerCount(roomMock, creepCountsByRoleMock);
+    
+        // assert
+        test.assertEqual(expected, result);
+    },
+}
