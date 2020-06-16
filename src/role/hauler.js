722aad5cbb6dfee21cf051895fad19406b28de1e
Joe Brady <joseph.b.brady@gmail.com>
2020-06-15 22:59:39 -0600

    changing haulers to fill spawn and extensions before towers

--- src/role/hauler.js
+++ src/role/hauler.js
@@ -10,7 +10,7 @@ module.exports = {
         // todo dry out this function        
         const towerFillFactor = 0.90;        
         const room = Game.rooms[creep.memory.homeRoom];
-        // fill spawns and extensions first, then towers
+        // fill spawns and extensions first, then towers, then storage
         let nonFullSpawnOrExtension = creep.pos.findClosestByPath(FIND_STRUCTURES, {
             filter: s => (s.room == room &&
                           (s.structureType == STRUCTURE_SPAWN ||
@@ -24,25 +24,17 @@ module.exports = {
             filter: s => s.room == room &&
                          s.structureType == STRUCTURE_STORAGE && 
                          _.sum(s.store) < s.storeCapacity });
-        let nonFullContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: s => s.room == room &&
-                         s.structureType == STRUCTURE_CONTAINER &&
-                         _.sum(s.store) < data.containerCapacity });        
-         if (nonFullTower != undefined) {
-            if (creep.transfer(nonFullTower, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(nonFullTower, { visualizePathStyle: { stroke: '#ffffff' } });
-            }
-        } else if (nonFullSpawnOrExtension != undefined) {
+         if (nonFullSpawnOrExtension != undefined) {
             if (creep.transfer(nonFullSpawnOrExtension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(nonFullSpawnOrExtension, { visualizePathStyle: { stroke: '#ffffff' } });
+            }
+        } else if (nonFullTower != undefined) {
+            if (creep.transfer(nonFullTower, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(nonFullTower, { visualizePathStyle: { stroke: '#ffffff' } });
             }
         } else if (nonFullStorage != undefined) {
             if (creep.transfer(nonFullStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(nonFullStorage, { visualizePathStyle: { stroke: '#ffffff' } });
-            }
-        } else if (nonFullContainer != undefined) {
-            if (creep.transfer(nonFullContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(nonFullContainer, { visualizePathStyle: { stroke: '#ffffff' } });
             }
         }
     }
