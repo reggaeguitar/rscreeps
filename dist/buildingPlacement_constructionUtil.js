2902013e3c4b337ce2a663d10f01b1a16bc5d632
Joe Brady <joseph.b.brady@gmail.com>
2020-06-19 20:39:56 -0600

    logging

--- dist/buildingPlacement_constructionUtil.js
+++ dist/buildingPlacement_constructionUtil.js
@@ -5,22 +5,26 @@ module.exports = {
         const roomTerrain = new Room.Terrain(room.name);
         
         function isValid(pos) {
+            // perf return as soon as false encountered
             // avoid squares where harvesters harvest (right next to a source)            
-            if (pos.getRangeTo(pos.findClosestByRange(FIND_SOURCES)) <= 1) return false;
-            if (storagePos != undefined && pos.x == storagePos.x && pos.y == storagePos.y) return false;
-            if (roomTerrain.get(pos.x, pos.y) == TERRAIN_MASK_WALL) return false;
-            let ret = true;
+            const closeToSource = (pos.getRangeTo(pos.findClosestByRange(FIND_SOURCES)) <= 1);
+            const isStoragePos = (storagePos != undefined && pos.x == storagePos.x && pos.y == storagePos.y);
+            const isTerrainMaskWall = (roomTerrain.get(pos.x, pos.y) == TERRAIN_MASK_WALL);
             // look returns an object for each thing on the position
+            let ret = !(closeToSource || isStoragePos || isTerrainMaskWall);
             pos.look().forEach(x => {
                 if (x.type == 'structure' || x.type == 'constructionSite') ret = false;                
             });
+            logger.log('in isValid', {ret, closeToSource, isStoragePos, isTerrainMaskWall });            
             return ret;
         }
         
         const potentialSite = (pos, dist, funcIndex) => {
             const newPos = mutatorFuncs[funcIndex](pos, dist);
+            console.log('a' + JSON.stringify(newPos));
             if (newPos.x <= 0 || newPos.y <= 0 || newPos.x >= 49 || newPos.y >= 49) return undefined;
             const newRoomPos = new RoomPosition(newPos.x, newPos.y, room.name);
+            console.log('b' + JSON.stringify(newRoomPos));
             if (!isValid(newRoomPos)) return undefined;
             return newPos;
         }
@@ -39,6 +43,7 @@ module.exports = {
             for (let distUnit = 0; distUnit < maxDistUnits; distUnit++) {
                 for (let funcIndex = 0; funcIndex < mutatorFuncs.length; funcIndex++) {
                     const newPos = potentialSite(spawn.pos, dist, funcIndex);
+                    console.log('newPos: ' + JSON.stringify(newPos));
                     if (newPos) return newPos;
             }
             dist += distIncrement;
