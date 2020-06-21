4740c375edea64578e5a9d8ca81912acbb77d6ed
Joe Brady <joseph.b.brady@gmail.com>
2020-06-20 18:28:25 -0600

    bug fix

--- dist/role_claimer.js
+++ dist/role_claimer.js
@@ -5,7 +5,8 @@ module.exports = {
         const room = Game.rooms[creep.memory.roomToClaim];
         logger.log('in claimer.run', { room, roomToClaim: creep.memory.roomToClaim });
         // todo handle attacking a controller if another player has a downgrade timer on it
-        if (creep.claimController(room.controller) == ERR_NOT_IN_RANGE) {
+        const controller = room.find(STRUCTURE_CONTROLLER)[0;]
+        if (creep.claimController(controller) == ERR_NOT_IN_RANGE) {
             creep.moveTo(room.controller);
         }        
     }
