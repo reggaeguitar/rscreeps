71e7d57b68f03ffa38eb00241027f8bda2f614b1
Joe Brady <joseph.b.brady@gmail.com>
2020-06-20 18:23:57 -0600

    logging

--- src/role/claimer.js
+++ src/role/claimer.js
@@ -1,7 +1,9 @@
+const logger = require('./logger');
 
 module.exports = {
     run: function(creep) {
         const room = Game.rooms[creep.memory.roomToClaim];
+        logger.log('in claimer.run', { room, roomToClaim: creep.memory.roomToClaim });
         // todo handle attacking a controller if another player has a downgrade timer on it
         if (creep.claimController(room.controller) == ERR_NOT_IN_RANGE) {
             creep.moveTo(room.controller);
