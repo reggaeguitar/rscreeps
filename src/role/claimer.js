a340615c241887ca71e31eb3360e98591c152386
Joe Brady <joseph.b.brady@gmail.com>
2020-06-20 18:30:04 -0600

    logging

--- src/role/claimer.js
+++ src/role/claimer.js
@@ -6,6 +6,7 @@ module.exports = {
         logger.log('in claimer.run', { room, roomToClaim: creep.memory.roomToClaim });
         // todo handle attacking a controller if another player has a downgrade timer on it
         const controller = room.find(STRUCTURE_CONTROLLER)[0];
+        logger.log('in claimer.run', { controller });
         if (creep.claimController(controller) == ERR_NOT_IN_RANGE) {
             creep.moveTo(room.controller);
         }        
